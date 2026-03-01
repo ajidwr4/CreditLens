11c9395a2ac7834fd716b31662471f6aed763917
Aji Dwira <117557476+ajidwr4@users.noreply.github.com>
2026-03-01 04:04:16 +0000

    minor

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  turbopack: false,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "@react-native-async-storage/async-storage": false,
+      "pino-pretty": false,
+    };
+    return config;
+  },
 };
 export default nextConfig;
