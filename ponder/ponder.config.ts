dd2b40e3aa33270bcc1d43a0181634b5583cc6bb
Aji Dwira <117557476+ajidwr4@users.noreply.github.com>
2026-03-03 02:23:21 +0000

    fix : Ponder config structure

--- ponder/ponder.config.ts
+++ ponder/ponder.config.ts
@@ -4,24 +4,24 @@ import { LendingMarketV3ABI } from "./abis/LendingMarketV3ABI";
 import { RealWorldCreditABI } from "./abis/RealWorldCreditABI";
 
 export default createConfig({
-  networks: {
+  chains: {
     creditcoin: {
-      chainId: 102031,
-      transport: http(process.env.PONDER_RPC_URL_102031),
+      id: 102031,
+      rpc: http(process.env.PONDER_RPC_URL ?? "https://rpc.cc3-testnet.creditcoin.network"),
     },
   },
   contracts: {
     LendingMarket: {
-      network: "creditcoin",
+      chain: "creditcoin",
       abi: LendingMarketV3ABI,
       address: "0xDD98f9D3aDC99e07A473bED4E396736d13117128", // v3
       startBlock: 4360000,
     },
     RealWorldCredit: {
-      network: "creditcoin",
+      chain: "creditcoin",
       abi: RealWorldCreditABI,
       address: "0xB6A2331289F2BeB040eF29bd1932f15Ae4f3771a",
-      startBlock: 4200000,
+      startBlock: 4329014,
     },
   },
 });
